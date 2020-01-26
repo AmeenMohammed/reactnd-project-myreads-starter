d91fda9f17a8d8d07fab0197e6a4d118396553ee
Ameen Mohammed <ameen.mohammed95by@gmail.com>
2020-01-27 01:35:50 +0200

    Add book shelf component

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import * as BooksAPI from './BooksAPI'
 import './App.css'
+import  BookShelf  from './BookShelf'
 
 class BooksApp extends React.Component {
   state = {
@@ -24,40 +25,15 @@ class BooksApp extends React.Component {
               </div>
               <div className="bookshelf">
                   <h2 className="bookshelf-title">Currently Reading</h2>
-                  <div className="bookshelf-books">
-                    <ol className="books-grid">
-                      {this.state.books.map((book) => (
-                        <li key={book.id}>   
-                         <div className="book">
-                          <div className="book-top">
-                            <div className="book-cover" style={{ width: 128, height: 192, backgroundImage: `url(${book.imageLinks.thumbnail})` }}></div>
-                            <div className="book-shelf-changer">
-                              <select>
-                                <option value="move" disabled>Move to...</option>
-                                <option value="currentlyReading">Currently Reading</option>
-                                <option value="wantToRead">Want to Read</option>
-                                <option value="read">Read</option>
-                                <option value="none">None</option>
-                              </select>
-                            </div>
-                          </div>
-                          <div className="book-title">{book.title}</div>
-                          <div className="book-authors">{book.authors}</div>
-                        </div>
-                        </li>
-                              ))}
-                    </ol>
-                  </div>
+                  <BookShelf books={this.state.books}/>
               </div>
               <div className="bookshelf">
                   <h2 className="bookshelf-title">Want to Read</h2>
-                  <div className="bookshelf-books">
-                  </div>
+                  <BookShelf books={this.state.books}/>
               </div>
               <div className="bookshelf">
                   <h2 className="bookshelf-title">Read</h2>
-                  <div className="bookshelf-books">
-                  </div>
+                  <BookShelf books={this.state.books}/>
               </div>
             </div>
       </div>
---
+++ src/BookShelf.js
@@ -0,0 +1,35 @@
+import React from 'react'
+import './App.css'
+
+class BookShelf extends React.Component {
+    render(){
+        return(
+            <div className="bookshelf-books">
+            <ol className="books-grid">
+              {this.props.books.map((book) => (
+                <li key={book.id}>   
+                 <div className="book">
+                  <div className="book-top">
+                    <div className="book-cover" style={{ width: 128, height: 192, backgroundImage: `url(${book.imageLinks.thumbnail})` }}></div>
+                    <div className="book-shelf-changer">
+                      <select value={book.shelf}>
+                        <option value="move" disabled>Move to...</option>
+                        <option value="currentlyReading">Currently Reading</option>
+                        <option value="wantToRead">Want to Read</option>
+                        <option value="read">Read</option>
+                        <option value="none">None</option>
+                      </select>
+                    </div>
+                  </div>
+                  <div className="book-title">{book.title}</div>
+                  <div className="book-authors">{book.authors}</div>
+                </div>
+                </li>
+                      ))}
+            </ol>
+          </div>
+        )
+    }
+}
+
+export default BookShelf
